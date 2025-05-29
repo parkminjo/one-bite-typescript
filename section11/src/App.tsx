bf8d65addc92dd1b7e193a71733225296d977c98
parkminjo <cruiser367@naver.com>
2025-05-29 13:44:23 +0900

    refactor: useReducer 적용

--- section11/src/App.tsx
+++ section11/src/App.tsx
@@ -1,25 +1,48 @@
-import { useRef, useState } from 'react';
+import { useReducer, useRef, useState } from 'react';
 import './App.css';
 import Editor from './components/Editor';
 import TodoItem from './components/TodoItem';
 import type { Todo } from './types';
 
+type Action =
+  | {
+      type: 'CREATE';
+      data: Todo;
+    }
+  | { type: 'DELETE'; targetId: number };
+
+const reducer = (state: Todo[], action: Action) => {
+  switch (action.type) {
+    case 'CREATE':
+      return [...state, action.data];
+    case 'DELETE':
+      return state.filter((todo) => todo.id !== action.targetId);
+    default:
+      return state;
+  }
+};
+
 function App() {
-  const [todos, setTodos] = useState<Todo[]>([]);
+  // const [todos, setTodos] = useState<Todo[]>([]);
+  const [todos, dispatch] = useReducer(reducer, []);
 
   const idRef = useRef(0);
 
   const addTodo = (text: string) => {
-    const newTodo = {
-      id: idRef.current++,
-      content: text,
-    };
-    setTodos((prev) => [...prev, newTodo]);
+    dispatch({
+      type: 'CREATE',
+      data: {
+        id: idRef.current++,
+        content: text,
+      },
+    });
   };
 
   const deleteTodo = (targetId: number) => {
-    const filteredTodos = todos.filter((todo) => todo.id !== targetId);
-    setTodos(filteredTodos);
+    dispatch({
+      type: 'DELETE',
+      targetId,
+    });
   };
 
   return (
